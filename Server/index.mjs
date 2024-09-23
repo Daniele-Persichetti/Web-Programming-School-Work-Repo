271fbc37603687ff7ed30d60c146150b97184d02
Daniele-Persichetti <157838597+Daniele-Persichetti@users.noreply.github.com>
2024-09-23 13:40:23 -0400

    Update index.mjs

--- Server/index.mjs
+++ Server/index.mjs
@@ -2,6 +2,7 @@
 import { createServer } from 'node:http';
 
 const PORT = 3000;
+const LOCALHOST = '127.0.0.1';
 
 const server = createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
@@ -9,7 +10,7 @@ const server = createServer((req, res) => {
 });
 
 // starts a simple http server locally on port 3000
-server.listen(PORT, '127.0.0.1', () => {
+server.listen(PORT, LOCALHOST, () => {
   console.log('Listening on 127.0.0.1:3000');
 });
 
