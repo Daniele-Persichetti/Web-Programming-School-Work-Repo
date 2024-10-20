57d7ebe23bb31eeff74087a58130e4bd671d35cf
Daniele-Persichetti <daniele.persichetti@hotmail.com>
2024-10-20 16:33:50 -0400

    Classwork + Update files

--- Client/src/pages/About.vue
+++ Client/src/pages/About.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Hello world!</p>
   </div>
 </template>
 
-<style>
-</style>
+<style></style>
--- Client/typed-router.d.ts
+++ Client/typed-router.d.ts
@@ -20,7 +20,8 @@ declare module 'vue-router/auto-routes' {
   export interface RouteNamedMap {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
     '/About': RouteRecordInfo<'/About', '/About', Record<never, never>, Record<never, never>>,
-    '/Index': RouteRecordInfo<'/Index', '/Index', Record<never, never>, Record<never, never>>,
+    '/Contact/': RouteRecordInfo<'/Contact/', '/Contact', Record<never, never>, Record<never, never>>,
     '/Jobs': RouteRecordInfo<'/Jobs', '/Jobs', Record<never, never>, Record<never, never>>,
+    '/products/': RouteRecordInfo<'/products/', '/products', Record<never, never>, Record<never, never>>,
   }
 }
